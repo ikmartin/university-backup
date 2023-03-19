6a30dcb3e6f6c4c157bafa4f90faa7604282269f
lwkatzen <isaac.act.martin@gmail.com>
2023-03-18 20:23:08 -0500

    asdf

--- code/rust/learning/chapt5/structs/target/debug/deps/structs-759f6fe5feb7245f.d
+++
@@ -1,5 +0,0 @@
-/home/isaac/Desktop/university/code/rust/learning/chapt5/structs/target/debug/deps/structs-759f6fe5feb7245f.rmeta: src/main.rs
-
-/home/isaac/Desktop/university/code/rust/learning/chapt5/structs/target/debug/deps/structs-759f6fe5feb7245f.d: src/main.rs
-
-src/main.rs:
--- code/rust/learning/chapt5/structs/target/debug/deps/structs-817de93a09f8de02.d
+++
@@ -1,5 +0,0 @@
-/home/isaac/Desktop/university/code/rust/learning/chapt5/structs/target/debug/deps/structs-817de93a09f8de02.rmeta: src/main.rs
-
-/home/isaac/Desktop/university/code/rust/learning/chapt5/structs/target/debug/deps/structs-817de93a09f8de02.d: src/main.rs
-
-src/main.rs:
