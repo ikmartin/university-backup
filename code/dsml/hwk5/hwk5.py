6d2d6aa1789aab6696beb91ed46ee047627b08aa
Isaac Martin <isaac@wireless-10-147-57-31.public.utexas.edu>
2023-04-15 12:21:18 -0500

    asdf

--- code/dsml/hwk5/hwk5.py
+++ code/dsml/hwk5/hwk5.py
@@ -20,6 +20,8 @@ def sgd(
     dtype="float64",
     random_state=None,
     plot_title="",
+    mul=1,
+    filename="",
 ):
     # Checking if the gradient is callable
     if not callable(gradient):
@@ -77,26 +79,27 @@ def sgd(
     # lecture 18 page 16
     def terms_in_bound(learn_rate):
         # get eigenvalues, n, and all norms of the rows of x
-        eig, _ = np.linalg.eig(np.linalg.inv(np.matmul(np.transpose(x), x)))
-        normsq = [np.linalg.norm(x[i, :]) ** 2 for i in range(x.shape[1])]
+        eig, _ = np.linalg.eig(np.matmul(x.T, x))
         n = x.shape[0]
+        row_norm = [(np.linalg.norm(x[j, :])) ** 2 for j in range(rows)]
 
         # get sigma
-        rows = x.shape[0]
-        row_norm = [np.linalg.norm(x[j, :]) ** 2 for j in range(rows)]
-        inp_diff = [np.abs(np.dot(x[j, :], ideal) - y[j]) ** 2 for j in range(n)]
-        sigma2 = n * sum([row_norm[j] - inp_diff[j] for j in range(n)])
+        inp_diff = [np.abs((np.dot(x[j, :], ideal) - y[j])) ** 2 for j in range(n)]
+        sigma2 = (
+            n ** (1.4)
+            * sum([row_norm[j] * inp_diff[j] for j in range(n)])
+            / (batch_size**2)
+        )[0]
 
         # get the constants L, mu, alpha etc
         mu = min(sorted(eig))
-        L = np.linalg.norm(
-            np.matmul(np.transpose(x), x), ord=2
-        )  # x.shape[0] * max(normsq)
+        L = n * max(row_norm)
 
-        if learn_rate >= 1 / (2 * L):
-            learn_rate = 1 / (2 * L)
+        if learn_rate >= 1 / (L):
+            alpha = 1 / (2 * L)
 
-        alpha = learn_rate
+        else:
+            alpha = learn_rate
         grad = gradient(x, y, ideal)
 
         print("alpha: ", alpha)
@@ -108,7 +111,7 @@ def sgd(
         coeff = 1 - 2 * alpha * mu * (1 - alpha * L)
         norm = np.linalg.norm(start - ideal) ** 2
         frac = alpha * (sigma2) / (mu * (1 - alpha * L))
-        print(f"alpha: {alpha},  alpha/(mu(1 - alpha*L)): {frac/sigma2}")
+        print(f"alpha: {alpha}\nalpha/(mu(1 - alpha*L)): {frac/sigma2}")
         return coeff, norm, frac
 
     def theory_bound(i, coeff, norm, frac):
@@ -120,6 +123,9 @@ def sgd(
         coeff, norm, frac = terms_in_bound(learn_rate=learn_rate)
         print(coeff, norm, frac)
 
+    accuracy.append(np.linalg.norm(start - ideal) ** 2)
+    theoretical.append(theory_bound(0, coeff, norm, frac))
+    iter_num.append(0)
     # Performing the gradient descent loop
     for i in range(n_iter):
         # Shuffle x and y
@@ -140,24 +146,40 @@ def sgd(
             # Updating the values of the variables
             vector += diff
 
-        acc = np.linalg.norm(vector - ideal)
-        print(f"completed iteration {i}, ||pred - b|| = {acc}")
+        acc = np.linalg.norm(vector - ideal) ** 2
+        # print(f"completed iteration {i}, ||pred - b|| = {acc}")
         if plot_title:
-            iter_num.append(i)
+            iter_num.append(i + 1)
             accuracy.append(acc)
-            theoretical.append(theory_bound(i * batch_size, coeff, norm, frac))
+            theoretical.append(theory_bound(i * mul, coeff, norm, frac))
 
     if plot_title:
         import matplotlib.pyplot as plt
 
         plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
-        plt.semilogy(iter_num, accuracy, c="b")
-        plt.semilogy(iter_num, theoretical, c="r")
+        plt.plot(iter_num, accuracy, c="b")
+        plt.plot(iter_num, theoretical, c="r")
         plt.xlabel("Iteration Number")
-        plt.ylabel(r"Criterion gap: $ \|w_t- w^*\| $")
+        plt.ylabel(r"Criterion gap: $ \|w_t- w^*\|^2 $")
         plt.title(plot_title)
-        plt.show()
+        plt.savefig(filename)
+        plt.close()
 
+    print("\n\nExperimental, Theoretical")
+    print(
+        accuracy[0],
+        "&",
+        accuracy[-1],
+        "&",
+        ((accuracy[0] - accuracy[-1]) / accuracy[0]) * 100,
+        "&",
+        theoretical[0],
+        "&",
+        theoretical[-1],
+        "&",
+        ((theoretical[0] - theoretical[-1]) / theoretical[0]) * 100,
+    )
+    print("\n")
     return vector if vector.shape else vector.item()
 
 
@@ -191,9 +213,16 @@ def problem3a():
     stepsize = 0.1
     batch_size = 100
     num_iter = 100
+    mul = 1000
 
     # graphing
-    titles = ["Title"]
+    titles = [
+        r"Fixed Var($A$), Var($\epsilon$) = 1",
+        r"Fixed Var($A$), Var($\epsilon$) = 0.1",
+        r"Fixed Var($A$), Var($\epsilon$) = 0.01",
+        r"Fixed Var($A$), Var($\epsilon$) = 0",
+    ]
+    filenames = ["graph1.png", "graph2.png", "graph3.png", "graph4.png"]
 
     def grad_loss(X, y, w):
         """
@@ -216,23 +245,92 @@ def problem3a():
         components = [stupid[j] + (Atb / n) for j in range(n)]
         return components
 
-    sgd(
-        gradient=grad_loss,
-        gradient_js=grad_loss_js,
-        x=A,
-        y=bs[3],
-        start=w_0,
-        learn_rate=stepsize,
-        ideal=w_min[3],
-        batch_size=batch_size,
-        n_iter=num_iter,
-        plot_title=titles[0],
-    )
+    for i in range(len(bs)):
+        b = bs[i]
+        w_best = w_min[i]
+        title = titles[i]
+        sgd(
+            gradient=grad_loss,
+            gradient_js=grad_loss_js,
+            x=A,
+            y=bs[i],
+            start=w_0,
+            learn_rate=stepsize,
+            ideal=w_min[i],
+            batch_size=batch_size,
+            n_iter=num_iter,
+            plot_title=titles[i],
+            mul=mul,
+            filename=filenames[i],
+        )
+
+
+def problem3b():
+    mu = 0
+    sigma = lambda j: 1 / math.sqrt(cols * j)
+    A = np.array([np.random.normal(mu, sigma(j), (cols)) for j in range(1, rows + 1)])
+
+    # setup the b and w vectors
+    M = np.matmul(np.linalg.inv(np.matmul(np.transpose(A), A)), np.transpose(A))
+    b0 = np.matmul(A, ones)
+    bs = [b0 + ep1, b0 + ep2, b0 + ep3, b0 + ep4]
+    w_min = [np.matmul(M, b) for b in bs]
+    w_0 = np.random.uniform(0, 1, size=(cols, 1))
+
+    # hyper-parameters
+    stepsize = 0.1
+    batch_size = 10
+    num_iter = 1000
+    mul = 1000
+
+    # graphing
+    titles = [
+        r"Varying Var($A$), Var($\epsilon$) = 1",
+        r"Varying Var($A$), Var($\epsilon$) = 0.1",
+        r"Varying Var($A$), Var($\epsilon$) = 0.01",
+        r"Varying Var($A$), Var($\epsilon$) = 0",
+    ]
+    filenames = ["graph5.png", "graph6.png", "graph7.png", "graph8.png"]
+
+    def grad_loss(X, y, w):
+        """
+        Gradient of the least squares loss function
+        """
+        return np.matmul(np.transpose(X), np.matmul(X, w) - y)
+
+    def grad_loss_js(X, y, w):
+        """
+        Gradient of the least squares loss function for the jth part
+        """
+        AtA = np.matmul(X.transpose(), X)
+        n = w.shape[0]
+        stupid = [AtA[:, j].reshape((n, 1)) * w[j] for j in range(n)]
+        Atb = np.matmul(X.transpose(), y)
+        print(AtA)
+        print(Atb)
+        print()
+
+        components = [stupid[j] + (Atb / n) for j in range(n)]
+        return components
 
     for i in range(len(bs)):
         b = bs[i]
         w_best = w_min[i]
         title = titles[i]
+        sgd(
+            gradient=grad_loss,
+            gradient_js=grad_loss_js,
+            x=A,
+            y=bs[i],
+            start=w_0,
+            learn_rate=stepsize,
+            ideal=w_min[i],
+            batch_size=batch_size,
+            n_iter=num_iter,
+            plot_title=titles[i],
+            mul=mul,
+            filename=filenames[i],
+        )
 
 
-problem3a()
+problem3b()
